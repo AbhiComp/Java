425e8cde1e6c67599ab13903c886ed858f322a5e
Abhishek Zanje <abhishekzanje.comp@gmail.com>
2024-11-03 19:28:42 +0530

    Merge Sort Modified

--- MergeSortDemo.java
+++ MergeSortDemo.java
@@ -1,64 +1,78 @@
 package projectdemo;
-
 import java.util.*;
-
 public class MergeSortDemo {
+	
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Enter the number of elements you want to enter in the array: ");
+		
+		int size=s.nextInt();
+		
+		int arr[]=new int[size];
+		
+		System.out.println("Enter the elements you want to enter in the array: ");
+		
+		for(int i=0;i<=size-1;i++) {
+			arr[i]=s.nextInt();
+		}
+		
+		mergeSort(arr,0,size-1);
+		
+		for(int x: arr)
+		System.out.print(x +" ");
+		
 
-    public static void mergeSort(int a[]) {
-        if (a.length < 2) {
-            return; 
-        }
-        
-        int mid = a.length / 2;
-        int[] left = Arrays.copyOfRange(a, 0, mid);
-        int[] right = Arrays.copyOfRange(a, mid, a.length);
-
-        mergeSort(left);  
-        mergeSort(right); 
-        merge(a, left, right); 
-    }
-
-    private static void merge(int[] result, int[] left, int[] right) {
-        int i = 0, j = 0, k = 0;
-
-   
-        while (i < left.length && j < right.length) {
-            if (left[i] <= right[j]) {
-                result[k++] = left[i++];
-            } else {
-                result[k++] = right[j++];
-            }
-        }
-
-       
-        while (i < left.length) {
-            result[k++] = left[i++];
-        }
-
-        
-        while (j < right.length) {
-            result[k++] = right[j++];
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        
-        System.out.println("Enter the number of elements you want to enter in the array: ");
-        int size = s.nextInt();
-        
-        int a[] = new int[size];
-        
-        System.out.println("Enter the elements you want to enter in the array: ");
-        for (int i = 0; i < size; i++) {
-            a[i] = s.nextInt();
-        }
-
-        mergeSort(a); 
-        
-        System.out.println("The Sorted array is: ");
-        for (int ele : a) {
-            System.out.print(ele + " "); 
-        }
-    }
+	}
+	
+	static void mergeSort(int arr[],int low,int high){
+		
+		if(low==high) {
+			return;
+		}
+		
+		int mid=(low+high)/2;
+		mergeSort(arr,low,mid);
+		mergeSort(arr,mid+1,high);
+		merge(arr,low,mid,high);
+				
+	}
+	
+	
+	static void merge(int [] arr,int low,int mid,int high) {
+		
+		Vector<Integer> vec=new Vector<Integer>();
+		
+		int left=low;
+		int right=mid+1;
+		while(left<=mid && right<=high) {
+			if(arr[left]<=arr[right]) {
+			vec.add(arr[left]);
+			left++;
+			}else {
+			vec.add(arr[right]);
+			right++;
+			}
+		}
+		
+		while(right<=high) {
+			vec.add(arr[right]);
+			right++;
+			}
+				
+		while(left<=mid) {
+			vec.add(arr[left]);
+			left++;
+		}
+		
+		for(int i=low;i<=high;i++) {
+			arr[i]=vec.get(i-low);
+		}
+		
+		
+	 }
+	
+	
 }
+
